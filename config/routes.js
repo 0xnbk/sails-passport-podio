c01ff8ec1b412a915baa0fead2305a0142216212
Nikhil Ben Kuruvilla <nbk@phases.dk>
2013-11-29 10:37:33 +0530

    added new routes for auth

--- config/routes.js
+++ config/routes.js
@@ -28,81 +28,89 @@
 
 module.exports.routes = {
 
-  // By default, your root route (aka home page) points to a view
-  // located at `views/home/index.ejs`
-  // 
-  // (This would also work if you had a file at: `/views/home.ejs`)
-  '/': {
-    view: 'home/index'
-  }
+    '/' : {
+        controller : 'home'
+    },
 
-  /*
-  // But what if you want your home page to display
-  // a signup form located at `views/user/signup.ejs`?
-  '/': {
-    view: 'user/signup'
-  }
+    '/login' : {
+        controller : 'auth',
+        action     : 'index'
+    },
 
+    '/logout' : {
+        controller : 'auth',
+        action     : 'logout'
+    }
 
-  // Let's say you're building an email client, like Gmail
-  // You might want your home route to serve an interface using custom logic.
-  // In this scenario, you have a custom controller `MessageController`
-  // with an `inbox` action.
-  '/': 'MessageController.inbox'
+    /*
+     // By default, your root route (aka home page) points to a view
+     // located at `views/home/index.ejs`
+     //
+     // (This would also work if you had a file at: `/views/home.ejs`)
+     '/': {
+     view: 'home/index'
+     }
 
-
-  // Alternatively, you can use the more verbose syntax:
-  '/': {
-    controller: 'MessageController',
-    action: 'inbox'
-  }
-
-
-  // If you decided to call your action `index` instead of `inbox`,
-  // since the `index` action is the default, you can shortcut even further to:
-  '/': 'MessageController'
+     // But what if you want your home page to display
+     // a signup form located at `views/user/signup.ejs`?
+     '/': {
+     view: 'user/signup'
+     }
 
 
-  // Up until now, we haven't specified a specific HTTP method/verb
-  // The routes above will apply to ALL verbs!
-  // If you want to set up a route only for one in particular
-  // (GET, POST, PUT, DELETE, etc.), just specify the verb before the path.
-  // For example, if you have a `UserController` with a `signup` action,
-  // and somewhere else, you're serving a signup form looks like: 
-  //
-  //		<form action="/signup">
-  //			<input name="username" type="text"/>
-  //			<input name="password" type="password"/>
-  //			<input type="submit"/>
-  //		</form>
-
-  // You would want to define the following route to handle your form:
-  'post /signup': 'UserController.signup'
+     // Let's say you're building an email client, like Gmail
+     // You might want your home route to serve an interface using custom logic.
+     // In this scenario, you have a custom controller `MessageController`
+     // with an `inbox` action.
+     '/': 'MessageController.inbox'
 
 
-  // What about the ever-popular "vanity URLs" aka URL slugs?
-  // (you might remember doing this with `mod_rewrite` in Apache)
-  //
-  // This is where you want to set up root-relative dynamic routes like:
-  // http://yourwebsite.com/twinkletoez
-  //
-  // NOTE:
-  // You'll still want to allow requests through to the static assets,
-  // so we need to set up this route to ignore URLs that have a trailing ".":
-  // (e.g. your javascript, CSS, and image files)
-  'get /*(^.*)': 'UserController.profile'
+     // Alternatively, you can use the more verbose syntax:
+     '/': {
+     controller: 'MessageController',
+     action: 'inbox'
+     }
 
-  */
+
+     // If you decided to call your action `index` instead of `inbox`,
+     // since the `index` action is the default, you can shortcut even further to:
+     '/': 'MessageController'
+
+
+     // Up until now, we haven't specified a specific HTTP method/verb
+     // The routes above will apply to ALL verbs!
+     // If you want to set up a route only for one in particular
+     // (GET, POST, PUT, DELETE, etc.), just specify the verb before the path.
+     // For example, if you have a `UserController` with a `signup` action,
+     // and somewhere else, you're serving a signup form looks like:
+     //
+     //		<form action="/signup">
+     //			<input name="username" type="text"/>
+     //			<input name="password" type="password"/>
+     //			<input type="submit"/>
+     //		</form>
+
+     // You would want to define the following route to handle your form:
+     'post /signup': 'UserController.signup'
+
+
+     // What about the ever-popular "vanity URLs" aka URL slugs?
+     // (you remember doing this with `mod_rewrite` in PHP)
+     //
+     // This is where you want to set up root-relative dynamic routes like:
+     // http://yourwebsite.com/twinkletoezz993
+     //
+     // You still want to allow requests through to the static assets,
+     // So we need to set up this route to allow URLs through that have a trailing ".":
+     // (e.g. your javascript, CSS, and image files)
+     'get /*(^.*)': 'UserController.profile'
+
+     */
 };
 
 
 
-/** 
- * (3) Action blueprints
- * These routes can be disabled by setting (in `config/controllers.js`):
- * `module.exports.controllers.blueprints.actions = false`
- *
- * All of your controllers ' actions are automatically bound to a route.  For example:
+/** (3) Action blueprints * * These routes can be disabled by setting( in config / controllers.js): * `module.exports.controllers.blueprints.actions = false` * * All of your controllers ' actions are automatically bound to a route.  For example:
  *   + If you have a controller, `FooController`:
  *     + its action `bar` is accessible at `/foo/bar`
  *     + its action `index` is accessible at `/foo/index`, and also `/foo`
@@ -110,7 +118,18 @@ module.exports.routes = {
 
 
 /**
- * (4) Shortcut CRUD blueprints
+ * (4) View blueprints
+ *
+ * These routes can be disabled by setting (in config/controllers.js):
+ *		`module.exports.views.blueprints = false`
+ *
+ * If you have a view file at `/views/foo/bar.ejs`, it will be rendered and served
+ * automatically via the route:  `/foo/bar`
+ *
+ */
+
+/**
+ * (5) Shortcut CRUD blueprints
  *
  * These routes can be disabled by setting (in config/controllers.js)
  *			`module.exports.controllers.blueprints.shortcuts = false`
@@ -128,7 +147,7 @@ module.exports.routes = {
  */
 
 /**
- * (5) REST blueprints
+ * (6) REST blueprints
  *
  * These routes can be disabled by setting (in config/controllers.js)
  *		`module.exports.controllers.blueprints.rest = false`
@@ -147,7 +166,7 @@ module.exports.routes = {
  */
 
 /**
- * (6) Static assets
+ * (7) Static assets
  *
  * Flat files in your `assets` directory- (these are sometimes referred to as 'public')
  * If you have an image file at `/assets/images/foo.jpg`, it will be made available
@@ -158,8 +177,6 @@ module.exports.routes = {
 
 
 /**
- * (7) 404 (not found) handler
- *
  * Finally, if nothing else matched, the default 404 handler is triggered.
  * See `config/404.js` to adjust your app's 404 logic.
  */
